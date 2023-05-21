ca6dcfea26a479a5485cb3af64feaf9100c8e82b
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-05-21 19:05:45 +0300

    done

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+new_person = Person.new
+
+new_person.talk
+
+new_person.walk
